cb6bd07f438c90e3b5199d5a9819b756934b0b9f
ZhengLi <2726401288@qq.com>
2022-10-30 16:02:54 +0800

    finish macro

---
+++ macro/Source.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/*
+MACRO is replace variable by stuff 
+
+#define SQUARE(X) X * X
+#define SQUARE(X) (X) * (X)
+
+*/
+
+#define DEBUG_PRINT(x, y, z) printf("File %s line %d: x = %d, y = %d, z = %d", __FILE__, __LINE__, x, y, z)
+
+//#define PRINT(FORMAT, VALUE) printf("The value is" FORMAT "\n", VALUE)
+#define PRINT(FORMAT, VALUE) printf("The value" #VALUE " is" FORMAT "\n", VALUE) //#argument is translated by the preprocessor into "argument"
+#define ADD_TO_SUM(sum_number, value) sum##sum_number += value 
+
+int main()
+{
+	int x = (1, 2);
+	int y = 3;
+	int z = 4;
+	int sum = 1;
+	int sum2 = 0;
+	ADD_TO_SUM(2, 2);
+	printf("%d", sum2);
+	PRINT("%d", y + 3);
+	return 0;
+}

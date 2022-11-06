a02df7fd160ecdc479fa94d306b44dc969b3c402
ZhengLi <2726401288@qq.com>
2022-11-06 10:31:42 +0800

    finish io

--- IO/Source.cpp
+++ IO/Source.cpp
@@ -3,6 +3,28 @@
 
 #pragma warning(disable: 4996)
 
+/*
+EOF -> 2 byte
+Character	getchar		pushchar
+Line		gets		puts
+			scanf		printf
+Binary		fread		fwrite
+
+*-Family Name-*-To any stream-*-Only stdin and stdout-*-String in Memory-*
+* getchar		fgetc, getc		getchar					Use a subscript or indirection on a pointer
+* putchar		fputc, putc		putchar					same above
+* gets			fgets			gets					Use strcpy to get/put lines to/from memory
+* puts			fputs			puts					same above
+* scanf			fscanf			scanf					sscanf
+* printf		fprintf			printf					sprintf
+*/
+
+/*
+ungetc: ungetc return a character previously read back to the stream so that it cn be read again later
+
+*/
+
+
 int main()
 {
 	FILE* file;

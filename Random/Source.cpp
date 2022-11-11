dcf872a169aadcba4a204294c15fa0944deb48f5
ZhengLi <2726401288@qq.com>
2022-11-11 15:44:22 +0800

    add time

--- Random/Source.cpp
+++ Random/Source.cpp
@@ -4,12 +4,15 @@
 
 int main()
 {
-	srand((unsigned int)time(NULL));
+	//srand((unsigned int)time(NULL));
 
-	for (int i = 0; i < 100; i++) {
-		printf("%d\r\n", rand());
-	}
-	printf("Max random num: %d", RAND_MAX); //32767
+	//for (int i = 0; i < 100; i++) {
+	//	printf("%d\r\n", rand());
+	//}
+	//printf("Max random num: %d", RAND_MAX); //32767
+
+	time_t t;
+	time_t t1 = time(&t);
 
 	return 0;
 }

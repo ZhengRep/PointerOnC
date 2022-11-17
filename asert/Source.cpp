f0f8cfa2c396b336cdf7a1df8b5c1325b79e8e05
ZhengLi <2726401288@qq.com>
2022-11-17 15:41:31 +0800

    add assert

---
+++ asert/Source.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+
+#pragma warning(disable: 4996)
+/*
+void abort(void);
+void atexit( void (func)(void) );
+void exit(int status);
+*/
+
+int main()
+{
+//	int* ptr = nullptr;
+//#define NDEBUG
+//	assert(ptr != nullptr); //only debug version is work
+
+	//char* env = getenv("path");
+
+	system("notepad.exe");
+	return 0;
+}
---
+++ signal/Source.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<signal.h>
+
+/*
+int raise(int sig);
+void (*signal(int si, void (*handler)(int)))(int);
+
+//print variable argument list
+//stdarg.h
+int vprintf(char const *format, va_list arg);
+int vfprintf(FILE *stream, char const *format, va_list arg);
+int vsprintf(char *buffer, FILE *stream, char const *format, va_list arg);
+
+*/
+
+int main()
+{
+	return 0;
+}
